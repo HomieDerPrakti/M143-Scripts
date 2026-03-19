e691a6ca7caa87310ce8e54af884113c0a3b34b6
HomieDerPrakti <julian.elias.egger@outlook.com>
2026-03-19 13:30:46 +0100

    add intermediary location

--- full_backup.sh
+++ full_backup.sh
@@ -6,4 +6,7 @@ DATE=$(date +'%Y-%m-%dT%H:%M:%s')
 SOURCE_DIR="/home/vmadmin/Patientenakten_Krankenhaus_Bern"
 TARGET_DIR="/data/backup/full-${DATE}/${DATE}-full"
 
-rsync -ac $SOURCE_IP:$SOURCE_DIR/ $TARGET_IP:$TARGET_DIR
+rm -rf /data/backup/*
+
+rsync -ac $SOURCE_IP:$SOURCE_DIR/ $TARGET_DIR
+rsync -ac $TARGET_DIR $TARGET_IP:$TARGET_DIR
--- restore_latest.sh
+++ restore_latest.sh
@@ -3,8 +3,11 @@
 SOURCE_IP="192.168.19.2"
 TARGET_IP="192.168.18.2"
 BACKUP_DIR="/data/backup"
-LAST_FULL_DIR='$(ssh $SOURCE_IP "find ${BACKUP_DIR} | sort | tail -n 1)'
-SOURCE_DIR='$(ssh $SOURCE_IP "find ${BACKUP_DIR} | sort | tail-n 1)'
+LAST_FULL_DIR='$(ssh $SOURCE_IP "find ${BACKUP_DIR} -maxdepth 1 -type d | sort | tail -n 1)'
+SOURCE_DIR='$(ssh $SOURCE_IP "find ${BACKUP_DIR} -maxdepth 1 -type d | sort | tail-n 1)'
 TARGET_DIR="/home/vmadmin/Patientenakten_Krankenhaus_Bern"
 
-rsync -ac $SOURCE_IP:$SOURCE_DIR/ $TARGET_IP:$TARGET_DIR
+rm -rf /data/backup/*
+
+rsync -ac $SOURCE_IP:$SOURCE_DIR/ /data/backup
+rsync -ac /data/backup/ $TARGET_IP:$TARGET_DIR
