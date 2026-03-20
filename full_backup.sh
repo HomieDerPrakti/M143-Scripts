0e3693b483b228dc0af17172a12432343193b185
HomieDerPrakti <julian.elias.egger@outlook.com>
2026-03-20 14:32:01 +0100

    (docs): added comments

--- full_backup.sh
+++ full_backup.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+
+# IP von Fileserver und Archivserver
 SOURCE_IP="192.168.18.2"
 TARGET_IP="192.168.19.2"
+
+# Timestamp für Labeling von Backup
 DATE=$(date +'%Y-%m-%dT%H:%M:%S')
+# verschlüsseltes und entschlüsseltes Backupverzeichnis
 BACKUP_DIR_ENC="/data/backup_encrypted"
 BACKUP_DIR="/data/backup_decrypted"
 
+# verschlüsseltes Verzeichnis entschlüsselt anhängen
 ssh $TARGET_IP "echo $BACKUP_DECRYPT_PW | gocryptfs -passfile /dev/stdin $BACKUP_DIR_ENC BACKUP_DIR"
 
+# Quell- und Zielverzeichnis
 SOURCE_DIR="/home/vmadmin/Patientenakten_Krankenhaus_Bern"
 TARGET_DIR="${BACKUP_DIR}/full-${DATE}/${DATE}-full"
 
+# Zwischenverzeichnis aufräumen
 rm -rf $BACKUP_DIR/*
 
+# Quelldaten in Zwischenverzeichnis ablegen
 rsync -ac --mkpath $SOURCE_IP:$SOURCE_DIR/ $TARGET_DIR
+# Daten von Zwischenverzeichnis in Zielverzeichnis ablegen
 rsync -ac --mkpath $TARGET_DIR/ $TARGET_IP:$TARGET_DIR
 
+# entschlüsseltes Backupverzeichnis abhängen
 ssh $TARGET_IP "fusermount -u $BACKUP_DIR"
--- inc_backup.sh
+++ inc_backup.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+# IP von Fileserver und Archivserver
 SOURCE_IP="192.168.18.2"
 TARGET_IP="192.168.19.2"
+# Timestamp für Labeling von Backup
 DATE=$(date +'%Y-%m-%dT%H:%M:%S')
-SOURCE_DIR="/home/vmadmin/Patientenakten_Krankenhaus_Bern"
+# verschlüsseltes und entschlüsseltes Backupverzeichnis
 BACKUP_DIR_ENC="/data/backup_encrypted"
 BACKUP_DIR="/data/backup_decrypted"
 
+# verschlüsseltes Backupverzeichnis entschlüsselt anhängen
 ssh $TARGET_IP "echo $BACKUP_DECRYPT_PW | gocryptfs -passfile /dev/stdin $BACKUP_DIR_ENC $BACKUP_DIR"
 
+# letztes Backup für --link-dest finden
 LAST_FULL_DIR=$(find $BACKUP_DIR -maxdepth 1 -type d | sort | tail -n 1)
 LAST_BACKUP=$(find $LAST_FULL_DIR -maxdepth 1 -type d | sort | tail -n 1)
+# Ziel- und Quellverzeichnis
 TARGET_DIR=${LAST_FULL_DIR}/${DATE}-inc
+SOURCE_DIR="/home/vmadmin/Patientenakten_Krankenhaus_Bern"
 
+# Quelldaten in Zwischenverzeichnis ablegen
 rsync -ac --mkpath --link-dest=$LAST_BACKUP $SOURCE_IP:$SOURCE_DIR/ $TARGET_DIR
+# Daten von Zwischenverzeichnis in Archiv ablegen
 rsync -aHc --mkpath $TARGET_DIR/ $TARGET_IP:$TARGET_DIR
 
+# entschlüsseltes Backupverzeichnis abhängen
 ssh $TARGET_IP "fusermount -u $BACKUP_DIR"
